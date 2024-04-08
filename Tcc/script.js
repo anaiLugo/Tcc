8a09fc77ed999a9dceeabe6fa555a0089fecf2d2
Anai Lugo <anailugo016@gmail.com>
2024-04-07 23:27:06 -0400

    cambio

--- Tcc/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("categoriesList");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
-
