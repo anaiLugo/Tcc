0a514dfbdd5d131cc1c3142c53fafbb8ed6793d4
Anai Lugo <anailugo016@gmail.com>
2024-04-13 20:39:45 -0400

    Actualización de Categorias

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function toggleMenu() {
-    var menu = document.getElementById("categoriesList");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
+    var categoriesList = document.getElementById('categoriesList');
+    categoriesList.style.display = categoriesList.style.display === 'block' ? 'none' : 'block';
 }
+
+function mostrarProductosPorCategoria(categoria) {
+    const productos = document.querySelectorAll('.producto');
+    productos.forEach(producto => {
+        if (categoria === 'todos' || producto.dataset.categoria === categoria) {
+            producto.style.display = ''; // en lugar de 'block', para volver al valor por defecto
+        } else {
+            producto.style.display = 'none';
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    mostrarProductosPorCategoria('todos');
+});
